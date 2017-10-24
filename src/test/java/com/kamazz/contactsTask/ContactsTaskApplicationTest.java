eafeeadbd8fbbbf57794869888b9107659adf125
kamazz <seryegin_o@ukr.net>
2017-10-24 19:34:14 +0300

    add annotation

--- src/test/java/com/kamazz/contactsTask/ContactsTaskApplicationTest.java
+++ src/test/java/com/kamazz/contactsTask/ContactsTaskApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DataJpaTest
 public class ContactsTaskApplicationTest {
     @Autowired
     private MockMvc mockMvc;
