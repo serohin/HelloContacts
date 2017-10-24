8589a4acd268e3ee6e37870560d3e5c961698abd
kamazz <seryegin_o@ukr.net>
2017-10-24 20:37:49 +0300

    change tests

--- src/test/java/com/kamazz/contactsTask/ContactsTaskApplicationTest.java
+++ src/test/java/com/kamazz/contactsTask/ContactsTaskApplicationTest.java
@@ -1,33 +1,45 @@
 package com.kamazz.contactsTask;
 
+import com.kamazz.contactsTask.controller.ContactController;
+import com.kamazz.contactsTask.entity.Contact;
+import com.kamazz.contactsTask.service.ContactServiceImpl;
+import com.kamazz.contactsTask.validator.RequestParamValidatorImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(ContactController.class)
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@DataJpaTest
 public class ContactsTaskApplicationTest {
+
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private ContactServiceImpl contactService;
+    @MockBean
+    private RequestParamValidatorImpl paramValidator;
+
     @Before
     public void setUp() throws Exception {
 
     }
+
     @After
     public void tearDown() throws Exception {
 
@@ -35,6 +47,13 @@ public class ContactsTaskApplicationTest {
 
     @Test
     public void getListContactsByRegex() throws Exception {
+        Contact abbot = new Contact((long) 9, "Abbott");
+        List<Contact> contactsNotMatchRegex = Arrays.asList(abbot);
+
+        given(this.contactService
+                .getContactsListNotMatchRegex("^.*[aelni].*$"))
+                .willReturn(contactsNotMatchRegex);
+
         this.mockMvc.perform(get("/hello/contacts?nameFilter=^.*[aelni].*$")
                 .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
                 .andExpect(status().isOk())
@@ -55,4 +74,5 @@ public class ContactsTaskApplicationTest {
                 .andExpect(status().isMethodNotAllowed())
                 .andExpect(content().string(containsString("Request method not supported")));
     }
+
 }
