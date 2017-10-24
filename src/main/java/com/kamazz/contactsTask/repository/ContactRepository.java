15b18ce35ca5f1d5f7918b7a6dfce7e9184b6628
kamazz <seryegin_o@ukr.net>
2017-10-24 16:36:06 +0300

    change Annotation

--- src/main/java/com/kamazz/contactsTask/repository/ContactRepository.java
+++ src/main/java/com/kamazz/contactsTask/repository/ContactRepository.java
@@ -4,7 +4,7 @@ import com.kamazz.contactsTask.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Component
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact,Long> {
 }
