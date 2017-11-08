f9633b6bd991f08cacf50006fbecb48fef452fb8
kamazz <seryegin_o@ukr.net>
2017-11-08 17:15:23 +0200

    Added 2 methods.

--- src/main/java/com/kamazz/contactsTask/service/ContactService.java
+++ src/main/java/com/kamazz/contactsTask/service/ContactService.java
@@ -1,10 +1,13 @@
 package com.kamazz.contactsTask.service;
 
 import com.kamazz.contactsTask.entity.Contact;
+import com.kamazz.contactsTask.repository.ContactRepository;
 
 import java.util.List;
 
 
 public interface ContactService {
     List<Contact> getContactsListNotMatchRegex(String regex);
+    void setContactRepository(ContactRepository mockRepository);
+    List<Contact> getAllContacts();
 }
